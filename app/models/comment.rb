702fe04ee36e819dbaeae3d2c4b967b48bdf65df
Jennie Louie <jennie.louie@gmail.com>
2013-10-27 00:22:16 -0700

    removed migration file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,5 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :date, :time_of_day, :street_comment, :username, :street_name, :walk_id
+  attr_accessible :date, :time_of_day, :street_comment, :username, :street_name
   belongs_to :walks
 
-  validates :username, presence: true
-  validates :street_name, presence: true
-  validates :street_comment, presence: true
-  validates :walk_id, presence: true
 end
